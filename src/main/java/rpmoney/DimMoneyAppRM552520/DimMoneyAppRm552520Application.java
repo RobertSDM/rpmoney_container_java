950a4591c6fbbf8cf91adea6390a67312eab91d7
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-25 07:49:29 -0300

    finish

--- src/main/java/rpmoney/DimMoneyAppRM552520/DimMoneyAppRm552520Application.java
+++ src/main/java/rpmoney/DimMoneyAppRM552520/DimMoneyAppRm552520Application.java
@@ -2,10 +2,18 @@ package rpmoney.DimMoneyAppRM552520;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DimMoneyAppRm552520Application {
-
+public class DimMoneyAppRm552520Application extends SpringBootServletInitializer {
+	@Override
+	// Configuring method has to be overridden
+	protected SpringApplicationBuilder
+	configure(SpringApplicationBuilder application){
+		return application.sources(
+				DimMoneyAppRm552520Application.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DimMoneyAppRm552520Application.class, args);
 	}
